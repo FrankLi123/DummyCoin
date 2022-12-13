3a687a23f2c9d5d8f5f7067a23c57eb9c2aadec9
FrankLi123 <75002277+FrankLi123@users.noreply.github.com>
2022-12-13 02:48:44 -0500

    deploy smart contract using web3.eth  module

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -9,23 +9,48 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 
-class Car{
-
-    function1(){
-        return "function 1 is successful";
-    }
-
-    function2(){
-        return "function 2 is successful";
-    }
-}
+const {interface, bytecode} = require('../compile');
 
 
-describe('testcase 1', ()=>{
 
-        it('test function 1', () =>{
 
-                const test = new Car();
-                assert.equal(test.function1(), 'function 1 is successful');
-        });
+
+let accounts;
+let inbox;
+
+// deploy the smart contract through web3.eth module
+beforeEach( async ()=>{
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hello!'] })
+    .send({ from: accounts[0], gas: '1000000'})
 });
+
+describe('Inbox', ()=>{
+    it('deploy contract', ()=>{
+        console.log(inbox);
+    })
+})
+
+// class Car{
+
+//     function1(){
+//         return "function 1 is successful";
+//     }
+
+//     function2(){
+//         return "function 2 is successful";
+//     }
+// }
+
+
+
+// describe('testcase 1', ()=>{
+
+//         it('test function 1', () =>{
+
+//                 const test = new Car();
+//                 assert.equal(test.function1(), 'function 1 is successful');
+//         });
+// });
