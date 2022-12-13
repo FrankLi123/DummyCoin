22a57d6262ef4da70b31ab6175a17a50d32e5187
FrankLi123 <75002277+FrankLi123@users.noreply.github.com>
2022-12-13 02:12:19 -0500

    added 1 test case of testing framework Mocha

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -4,5 +4,28 @@
 */
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const { Test } = require('mocha');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+
+class Car{
+
+    function1(){
+        return "function 1 is successful";
+    }
+
+    function2(){
+        return "function 2 is successful";
+    }
+}
+
+
+describe('testcase 1', ()=>{
+
+        it('test function 1', () =>{
+
+                const test = new Car();
+                assert.equal(test.function1(), 'function 1 is successful');
+        });
+});
