645973ece9d55b4105c3ddd9eb6ee65f450f7663
FrankLi123 <75002277+FrankLi123@users.noreply.github.com>
2022-12-15 00:10:59 -0500

    deployed the contract on web3(in Gorli)

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -12,5 +12,20 @@ const provider = new HDWalletProvider(
 //make new Web3 instance - use methods of smart contract for testing
 const web3 = new Web3(provider);
 
+const deploy = async()=>{
+
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments:['Hello !']})
+    .send({gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
+
 
 
