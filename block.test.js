0617435679dc86baff7fa497529aba5824cee07b
FrankLi123 <zoli@ucsd.edu>
2023-07-01 19:49:41 -0400

    Implemented hash function and hash test cases with SHA-256 ; Created MinedBlock function

--- block.test.js
+++ block.test.js
@@ -1,14 +1,17 @@
 const Block = require('./block');
+const {GENESIS_DATA} = require('./config');
+
+const cryptoHash = require('./crypto-hash');
+
 
 describe('Block', () => {
 
 
     const timestamp = 'foo-date';
     const lastHash = 'foo-hash';
-
     const hash = 'bar-hash';
-
     const data = ['blockchain', 'data'];
+
 
     const block = new Block({ timestamp, lastHash,hash, data });
 
@@ -18,6 +21,68 @@ describe('Block', () => {
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
     });
-});
+
+
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('return a Block isntance', ()=>{
+            expect(genesisBlock instanceof Block).toEqual(true);
+        });
+
+        it('returns the genesis data', ()=>{
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+
+    });
+
+
+
+    // for creating mine block
+
+    describe('mineBlock()', () => {
+
+        const lastBlock = Block.genesis();
+        const data = 'mined data';
+        const minedBlock = Block.minedBlock({
+            lastBlock, data
+        });
+
+
+
+        it('return a Block isntance', ()=>{
+            expect(minedBlock instanceof Block).toEqual(true);
+        });
+
+
+        it('set the `lastHash` to be the `hash` of the lastBlock', () => {
+
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+
+        });
+
+        it('set the `data` ', () => {
+
+            expect(minedBlock.data).toEqual(data);
+
+        });
+
+        it('set a `timestamp`', ()=> {
+
+            expect(minedBlock.timestamp).not.toEqual(undefined);
+       
+        });
+
+
+        it('creates a SHA-256 `hash` based on the proper inputs', () => {
+
+            expect(minedBlock.hash).toEqual(cryptoHash( minedBlock.timestamp, lastBlock.hash, data ) );
+
+        });
+
+    })
+
+}); 
 
 
---
+++ config.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {
+    timestamp: 1,
+    lastHash: '_____',
+    hash: 'hash_one',
+    data:[]    
+};
+
+module.exports = { GENESIS_DATA }
