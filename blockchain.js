81216f56f8e93c3aa6b64b5dc1f40f9ff7a01271
FrankLi123 <zoli@ucsd.edu>
2023-07-02 23:58:12 -0400

    Implemented the func 'isValidChain()'

--- blockchain.js
+++ blockchain.js
@@ -16,22 +16,45 @@ class blockchain{
     }
 
     addBlock( {data} ){
-
-        const newBlock =             Block.minedBlock({
-
+        const newBlock = Block.minedBlock({
             lastBlock: this.chain[this.chain.length - 1],
             data
-
         });
-
         this.chain.push( 
-
             newBlock
         );
+    };
 
-    }
 
-}
+    /* Check whether the blockchain is valid.
+    */
+    static isValidChain( blockchain ){
+
+        // check the data of the first block is correct
+        if(blockchain[0] != Block.genesis()){
+            return false;
+        }
+
+
+        //check if a lastHash reference of a block has been changed.
+
+        let previousHash = blockchain[0].hash;
+
+        for(let i = 1 ; i  < blockchain.length; i++){
+
+                if(blockchain[i].lastHash != previousHash){
+
+                    return false;
+                }
+                previousHash = blockchain[i].hash;
+        }
+    
+
+        
+        return true;
+    };
+
+};
 
 
 module.exports = blockchain;
