18240e51ac53646a48e6e69377a9313666230fbb
FrankLi123 <zoli@ucsd.edu>
2023-07-06 23:14:20 -0400

    Post Request to mine a new Block

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
+const bodyParser = require('body-parser');
 
 const app = express();
 const blockchain = new Blockchain();
 
+
+app.use(bodyParser.json());
+
 // send GET request to other server ?
 app.get('/api/blocks', ( request, response) =>{
 
-    request.json(blockchain.chain);
+    response.json(blockchain.chain);
+
+});
+
+app.post('/api/mine', (req, res) => {
+
+    const {data} = req.body;
+
+    
+    blockchain.addBlock({data});
+
+    // redirect to block display page
+    res.redirect('/api/blocks');
 
 });
 
