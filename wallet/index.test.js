9069405e2b8f567442a7bf74f15052bf02917452
FrankLi123 <zoli@ucsd.edu>
2023-07-07 23:59:26 -0400

    Impleted the Wallet Class and the Wallet Test class (no function yet)

---
+++ wallet/index.test.js
@@ -0,0 +1,32 @@
+const Wallet = require('./index');
+
+
+describe('Wallet', () => {
+
+    let wallet;
+
+
+    beforeEach( () => {
+
+        wallet = new Wallet();
+
+    });
+
+
+    // check whether or not it has balance
+
+    it ('wallet has a `balance`', () =>{
+
+        expect(wallet).toHaveProperty('balance');
+
+    });
+
+
+    it('wallet has a `public key`', () => {
+
+        expect(wallet).toHaveProperty('publicKey');
+    });
+
+
+});
+
