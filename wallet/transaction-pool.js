d8b12f1aa575d9645cb5ca491e445ec6fcb28cf0
FrankLi123 <zoli@ucsd.edu>
2023-07-13 23:24:21 -0400

    Create the class transaction-pool.js

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,15 @@
+class TransactionPool{
+
+
+    constructor(){
+
+        this.transactionMap = {};
+    }
+
+    setTransaction(transaction){
+        this.transactionMap[transaction.id] = transaction;
+    }
+
+}
+
+module.exports = TransactionPool;
